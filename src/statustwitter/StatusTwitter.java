54022497a35c84c6d07d8b924e089d5a9cc91db6
Alex Urcera <Alex Urcera@Alex>
2015-03-19 09:46:45 +0100

    Commit de la branch

--- src/statustwitter/StatusTwitter.java
+++ src/statustwitter/StatusTwitter.java
@@ -6,6 +6,7 @@ import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
 
 /**
  * Clase para recobrar los twitts del morrazo
@@ -20,7 +21,14 @@ public class StatusTwitter {
     public static void main(String[] args) throws TwitterException {
      
         Twitter mitwitter = new TwitterFactory().getInstance();
- 
+ ConfigurationBuilder cb = new ConfigurationBuilder();
+cb.setDebugEnabled(true)
+  .setOAuthConsumerKey("VoSmbUz4KtX6LWMADG2wjcai4")
+                .setOAuthConsumerSecret("l6UmkUXGVp7kARYmNGB0of5PsVuelFaokEm9rI4v5Fsjb9KZ7U")
+                .setOAuthAccessToken("3055290010-QWLhHK12ERY0t18MVAXQk4EfINP44CO9LtFP2Xq")
+                .setOAuthAccessTokenSecret("JsDAHOzJOk0YPlXQn56WOM6zpzei2nKvd4m1VerbVT2Gf");
+TwitterFactory tf = new TwitterFactory(cb.build());
+Twitter twitter = tf.getInstance();
         
         Query query = new Query("#Cangas");
         QueryResult result = mitwitter.search(query);
